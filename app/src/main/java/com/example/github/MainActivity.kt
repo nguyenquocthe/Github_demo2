bc61635469a102d33d6e1d969f53ea8151d2ebfc
nguyenthe <nguyenthe23031998@gmail.com>
2021-07-05 22:52:54 +0700

    change activity

--- app/src/main/java/com/example/github/MainActivity.kt
+++ app/src/main/java/com/example/github/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.github
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
                 Log.d("AA","nguyen quoc the")
+                Log.d("BBB","dggfdgdf")
     }
 }
